7364b6e6919ca1f57dccd5c9df6065f430c8ad87
wiktorpp <grazynapp65@gmail.com>
2021-10-09 11:57:17 +0200

    Deleted unwanted files

--- pebblesay-gtk.py
+++ pebblesay-gtk.py
@@ -2,6 +2,8 @@ import gi
 gi.require_version("Gtk", "3.0")
 from gi.repository import Gtk
 
+import pebblesay
+
 class Handler:
     def onDestroy(self, *args):
         Gtk.main_quit()
@@ -11,7 +13,7 @@ class Handler:
         return lambda self, *args: print(f'Handler "{name}" was called by a {self.get_name()}')
 
 builder = Gtk.Builder()
-builder.add_from_file("ui.glade")
+builder.add_from_file("pebblesay-gtk.glade")
 builder.connect_signals(Handler())
 
 window = builder.get_object("window")
